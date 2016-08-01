bb40dfadac47779df5b7af3bcc394276ac8dbd0b
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-08-01 19:06:56 +0200

    Django project bootstrap

---
+++ django-manager/manager/settings/dev.py
@@ -0,0 +1,21 @@
+from .base import *
+
+
+# removing security enforcement in development mode
+DEBUG = True
+SECRET_KEY = env('DJANGO_SECRET_KEY', '1234567890')
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
+
+# enabling console loggers
+LOGGING['loggers'] = {
+    'django': {
+        'handlers': ['console'],
+        'level': env('DJANGO_LOG_LEVEL', 'INFO'),
+    },
+    'manager': {
+        'handlers': ['console'],
+        'level': env('MANAGER_LOG_LEVEL', 'DEBUG'),
+    },
+}
