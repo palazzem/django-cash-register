d5677fcf0b2ef4ac78cbbba95c4ffd2bc7099317
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-08-01 20:52:52 +0200

    add Recipe and the ManyToMany relationship with Product

--- django-manager/registers/admin.py
+++ django-manager/registers/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Recipe, Sell
+
+
+class SellInline(admin.TabularInline):
+    model = Sell
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [
+        SellInline,
+    ]
 
 
 admin.site.register(Product)
--- django-manager/registers/models.py
+++ django-manager/registers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import formats
 
 from djmoney.models.fields import MoneyField
 
@@ -16,3 +17,30 @@ class Product(models.Model):
 
     def __str__(self):
         return '{} ({})'.format(self.name, self.price)
+
+
+class Recipe(models.Model):
+    """
+    Recipe model that aggregates a set of products and that
+    creates the proper commands to print the Recipe.
+    """
+    date = models.DateTimeField(auto_now_add=True)
+    products = models.ManyToManyField('Product', through='Sell')
+
+    def __str__(self):
+        return formats.date_format(self.date, 'DATETIME_FORMAT')
+
+
+class Sell(models.Model):
+    """
+    ManyToMany relationship that includes all extra fields
+    for the Recipe-Product relation. It provides:
+        * the quantity of sold items
+    """
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=10, decimal_places=3)
+    price = MoneyField(max_digits=10, decimal_places=2, default_currency='EUR')
+
+    def __str__(self):
+        return '{}x{} sold {}'.format(self.product, self.quantity, self.recipe)
