73d3f9cd95cf61068f73dfe22c8ddad81c57d17b
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-08-02 22:16:05 +0200

    authentication required for products endpoint

--- django-manager/registers/apiviews.py
+++ django-manager/registers/apiviews.py
@@ -1,5 +1,5 @@
-from rest_framework import mixins
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -10,5 +10,7 @@ class ProductViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     The ``ProductViewSet`` API, provides only the list of the configured
     products, and doesn't allow any [C-UD] interaction.
     """
+    permission_classes = (IsAuthenticated,)
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
