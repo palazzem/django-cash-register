9f74d5204a8bd6d0d3a5b18ca8f4d6603220c9d2
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-08-03 17:53:40 +0200

    add test for ProductViewSet (list and authentication)

--- django-manager/registers/apiviews.py
+++ django-manager/registers/apiviews.py
@@ -1,5 +1,5 @@
 from rest_framework import mixins, viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -10,7 +10,7 @@ class ProductViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     The ``ProductViewSet`` API, provides only the list of the configured
     products, and doesn't allow any [C-UD] interaction.
     """
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAdminUser,)
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
---
+++ django-manager/tests/test_apiviews.py
@@ -0,0 +1,72 @@
+import pytest
+
+from model_mommy import mommy
+
+from django.core.urlresolvers import reverse
+
+from registers.models import Product
+
+
+@pytest.mark.django_db
+def test_product_api_ok(api_client, django_user_model):
+    """
+    Alice is the shop owner (admin), that uses an applications to
+    retrieve registered products.
+        * Alice is a valid user
+        * Alice is a super user
+        * Alice retrieves the products list
+        * the products list is returned (200)
+    """
+    # create some products
+    products = mommy.make(Product, _quantity=2)
+    # Alice is an admin user
+    alice = django_user_model.objects.create_superuser(
+        username='alice',
+        email='alice@shop.com',
+        password='123456',
+    )
+    api_client.login(username='alice', password='123456')
+    # get the products endpoint
+    endpoint = reverse('registers:product-list')
+    response = api_client.get(endpoint)
+    # authorized with two products as a response
+    assert response.status_code == 200
+    assert response.data[0]['name'] == products[0].name
+    assert response.data[1]['name'] == products[1].name
+
+
+@pytest.mark.django_db
+def test_product_api_unauthorized_for_regular(api_client, django_user_model):
+    """
+    Bob is a regular user, that wants to retrieve registered products.
+    Unfortunately, the endpoint is available only for admin users and
+    because of that, he cannot access the API
+        * Bob is an authenticated user
+        * Bob is a regular user
+        * Bob retrieves the products list
+        * the products list is not returned (403)
+    """
+    # Bob is a regular user
+    bob = django_user_model.objects.create_user(username='bob', password='123456')
+    api_client.login(username='bob', password='123456')
+    # get the products endpoint
+    endpoint = reverse('registers:product-list')
+    response = api_client.get(endpoint)
+    # authorized
+    assert response.status_code == 403
+
+
+def test_product_api_unauthorized_for_anonymous(api_client):
+    """
+    Eve is a malicious user, that uses a custom client to
+    retrieve Alice's registered products. Because Eve is not
+    a valid user, she must receive a 403
+        * Eve is not a registered user
+        * Eve retrieves the products list
+        * the products list is not returned (403)
+    """
+    # get the products endpoint
+    endpoint = reverse('registers:product-list')
+    response = api_client.get(endpoint)
+    # unauthorized
+    assert response.status_code == 403
