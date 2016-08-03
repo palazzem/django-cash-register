39feeaa6a0407823e48c5fc70a1dce03bbeb67ef
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-08-03 15:47:31 +0200

    add RecipeSerializer and RecipeItemSerializer; provide tests for base cases and empty values

--- django-manager/registers/serializers.py
+++ django-manager/registers/serializers.py
@@ -1,11 +1,50 @@
-from rest_framework.serializers import ModelSerializer
+from django.conf import settings
+
+from rest_framework import serializers
+
+from djmoney.contrib.django_rest_framework.fields import MoneyField
 
 from .models import Product
 
 
-class ProductSerializer(ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     """
     Serializer for the Product model
     """
     class Meta:
         model = Product
+
+
+class RecipeItemSerializer(serializers.Serializer):
+    """
+    The ``RecipeItemSerializer`` serializes a single line
+    item of a generic Recipe. Each row must contain:
+        * ``id``: the product primary key that should exist
+          in the database, otherwise it is not valid
+        * ``price``: how much is the price of a single item;
+          it should NOT be considered as the total amount
+          of this row, because the moltiplication is handled
+          internally
+        * ``quantity``: how many items are bought; the field
+          is optional and could be omitted if the quantity
+          is one
+    """
+    id = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+    price = MoneyField(max_digits=10, decimal_places=2)
+    price_currency = serializers.ChoiceField(settings.CURRENCIES, default='EUR')
+    quantity = serializers.DecimalField(max_digits=10, decimal_places=2, default=1.0)
+
+
+class RecipeSerializer(serializers.Serializer):
+    """
+    The ``RecipeSerializer`` serializes a list of products
+    so that they can be used to create a new recipe. Anyway,
+    it will not accept directly a ``Recipe`` model because
+    other fields should not be set by the user input.
+
+    This serializer creates a new ``Recipe`` and then all
+    related ``Sell`` instances. Furthermore, it ensures
+    that the given input is also validated for the
+    ``python-cash-register`` package
+    """
+    products = RecipeItemSerializer(many=True, allow_empty=False)
--- django-manager/tests/test_serializers.py
+++ django-manager/tests/test_serializers.py
@@ -1,6 +1,9 @@
 import pytest
 
-from registers.serializers import ProductSerializer
+from model_mommy import mommy
+
+from registers.models import Product
+from registers.serializers import ProductSerializer, RecipeItemSerializer, RecipeSerializer
 
 
 class TestProduct:
@@ -35,3 +38,112 @@ class TestProduct:
         # check the serializer
         serializer = ProductSerializer(data=product)
         assert serializer.is_valid() is True
+
+
+class TestRecipe:
+    @pytest.mark.django_db
+    def test_item_serializer(self):
+        """
+        Test the ``RecipeItemSerializer``; it doesn't contain
+        the ``description`` field, even if it's required to
+        print a valid invoice
+        """
+        # a product
+        product = mommy.make(Product)
+        # a serialized item
+        item = {
+            'id': product.id,
+            'price': '5.90',
+            'price_currency': 'EUR',
+            'quantity': '10.0',
+        }
+        # check the serializer
+        serializer = RecipeItemSerializer(data=item)
+        assert serializer.is_valid() is True
+
+    @pytest.mark.django_db
+    def test_item_serializer_defaults(self):
+        """
+        Test the ``RecipeItemSerializer`` optional attributes
+        and defaults
+        """
+        # a product
+        product = mommy.make(Product)
+        # a serialized item
+        item = {
+            'id': product.id,
+            'price': '5.90',
+        }
+        # check the serializer
+        serializer = RecipeItemSerializer(data=item)
+        assert serializer.is_valid() is True
+        assert serializer.validated_data['price_currency'] == 'EUR'
+        assert serializer.validated_data['quantity'] == 1.0
+
+    @pytest.mark.django_db
+    def test_item_serializer_wrong_currency(self):
+        """
+        Test the ``RecipeItemSerializer`` with a wrong / not supported currency
+        """
+        # a product
+        product = mommy.make(Product)
+        # a serialized item
+        item = {
+            'id': product.id,
+            'price': '5.90',
+            'price_currency': 'USD',
+        }
+        # check the serializer
+        serializer = RecipeItemSerializer(data=item)
+        assert serializer.is_valid() is False
+        assert len(serializer.errors) == 1
+        assert serializer.errors['price_currency'][0] == '"USD" is not a valid choice.'
+
+    @pytest.mark.django_db
+    def test_recipe_serializer(self):
+        """
+        Test the ``RecipeSerializer`` so that a list of products creates a new
+        ``Recipe`` object with bought items:
+            * create 3 random products
+            * sell each product using different prices and quantities
+            * the serializer must be valid
+        """
+        # a list of product
+        products = mommy.make(Product, _quantity=3)
+        # products that are sold
+        recipe = {
+            'products': [
+                {
+                    'id': products[0].id,
+                    'price': '5.90',
+                },
+                {
+                    'id': products[1].id,
+                    'price': '2.00',
+                    'quantity': '2.0',
+                },
+                {
+                    'id': products[2].id,
+                    'price': '1.00',
+                    'quantity': '1.68',
+                },
+            ]
+        }
+        # check the serializer
+        serializer = RecipeSerializer(data=recipe)
+        assert serializer.is_valid() is True
+
+    @pytest.mark.django_db
+    def test_recipe_serializer_empty_list(self):
+        """
+        Ensure that the ``RecipeSerializer`` doesn't create a ``Recipe`` object
+        if the list of products is empty.
+        """
+        # use the serializer with an empty list of products
+        recipe = {
+            'products': []
+        }
+        # check the serializer
+        serializer = RecipeSerializer(data=recipe)
+        assert serializer.is_valid() is False
+        assert serializer.errors['products']['non_field_errors'][0] == 'This list may not be empty.'
