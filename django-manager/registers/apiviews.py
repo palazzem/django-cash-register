917d63d70a4409ddfa4f0275cfcc4939b13eb7d2
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-08-03 20:33:01 +0200

    add the RecipeViewSet that uses the custom serializer

--- django-manager/registers/apiviews.py
+++ django-manager/registers/apiviews.py
@@ -1,8 +1,8 @@
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAdminUser
 
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Recipe
+from .serializers import ProductSerializer, RecipeSerializer
 
 
 class ProductViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
@@ -14,3 +14,17 @@ class ProductViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class RecipeViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    """
+    The ``RecipeViewSet`` API provides an endpoint to create a new Recipe
+    according to given products. Indeed the API is not related to a
+    particular model but only makes use of a custom ``RecipeSerializer``
+    to store the new ``Recipe`` while printing a new recipe using a
+    connected device.
+    """
+    permission_classes = (IsAdminUser,)
+
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
--- django-manager/registers/urls.py
+++ django-manager/registers/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 
 from rest_framework.routers import DefaultRouter
 
-from .apiviews import ProductViewSet
+from .apiviews import ProductViewSet, RecipeViewSet
 
 
 app_name = 'registers'
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
+router.register(r'recipes', RecipeViewSet)
 urlpatterns = router.urls
