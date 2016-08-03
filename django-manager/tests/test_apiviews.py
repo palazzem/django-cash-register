efac3ad44f142ec0952c9eb46fb3d69c310161f9
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-08-03 20:35:45 +0200

    add tests for ReceiptViewSet

--- django-manager/tests/test_apiviews.py
+++ django-manager/tests/test_apiviews.py
@@ -4,7 +4,7 @@ from model_mommy import mommy
 
 from django.core.urlresolvers import reverse
 
-from registers.models import Product
+from registers.models import Product, Recipe
 
 
 @pytest.mark.django_db
@@ -36,7 +36,7 @@ def test_product_api_ok(api_client, django_user_model):
 
 
 @pytest.mark.django_db
-def test_product_api_unauthorized_for_regular(api_client, django_user_model):
+def test_product_api_unauthorized_for_regular_user(api_client, django_user_model):
     """
     Bob is a regular user, that wants to retrieve registered products.
     Unfortunately, the endpoint is available only for admin users and
@@ -70,3 +70,50 @@ def test_product_api_unauthorized_for_anonymous(api_client):
     response = api_client.get(endpoint)
     # unauthorized
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_recipe_api_ok(api_client, django_user_model):
+    """
+    Alice is the shop owner (admin), that uses an applications to
+    create a new recipe.
+        * Alice is a valid user
+        * Alice is a super user
+        * Alice applicastions posts a list of products to create
+          a new recipe
+        * the recipe and the products are sold correctly
+    """
+    # create some products
+    products = mommy.make(Product, _quantity=3)
+    # Alice is an admin user
+    alice = django_user_model.objects.create_superuser(
+        username='alice',
+        email='alice@shop.com',
+        password='123456',
+    )
+    api_client.login(username='alice', password='123456')
+    # sold products
+    sold_items = {
+        'products': [
+            {
+                'id': products[0].id,
+                'price': '5.90',
+            },
+            {
+                'id': products[1].id,
+                'price': '2.00',
+                'quantity': '2.0',
+            },
+            {
+                'id': products[2].id,
+                'price': '1.00',
+                'quantity': '1.68',
+            },
+        ]
+    }
+    # get the receipts endpoint
+    endpoint = reverse('registers:recipe-list')
+    response = api_client.post(endpoint, data=sold_items)
+    # the recipe has been created through the ``RecipeSerializer``
+    assert response.status_code == 201
+    assert Recipe.objects.count() == 1
