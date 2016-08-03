1b8c2a2b0606e29d688f2bdcb3f9b7c2dd59e28a
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-08-03 17:53:08 +0200

    add registers namespace

--- django-manager/registers/urls.py
+++ django-manager/registers/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 from .apiviews import ProductViewSet
 
 
+app_name = 'registers'
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
 urlpatterns = router.urls
