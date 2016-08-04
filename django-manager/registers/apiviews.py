87c60af0249fb83551d88561256710ce1ad3a4cd
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-08-04 13:31:26 +0200

    override perform_create for RecipeViewSet; it creates a Recipe and prints the receipt

--- django-manager/registers/apiviews.py
+++ django-manager/registers/apiviews.py
@@ -1,7 +1,10 @@
+from django.conf import settings
+
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAdminUser
 
 from .models import Product, Recipe
+from .receipts import convert_serializer, print_receipt
 from .serializers import ProductSerializer, RecipeSerializer
 
 
@@ -28,3 +31,22 @@ class RecipeViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
 
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
+    def perform_create(self, serializer):
+        """
+        Save the serializer so that the ``Recipe`` and connected models
+        are created, but also prints the receipt if the ``REGISTER_PRINT``
+        setting is set to ``True``.
+
+        If the variable is set to ``False``, the model is created without printing;
+        in a real world example, ``False`` must be used only for development
+        mode.
+        """
+        # create the ``Recipe`` model, honoring the ManyToMany
+        serializer.save()
+
+        if settings.REGISTER_PRINT:
+            # convert serializer validated_data and send it
+            # to the cash register printer
+            data = convert_serializer(serializer)
+            print_receipt(data)
