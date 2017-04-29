4e70667891e7548c7c53bd0299ed26fc7a47b566
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-04-30 01:24:22 +0200

    [core] add RedirectView for the / URL

--- django-manager/manager/urls.py
+++ django-manager/manager/urls.py
@@ -1,11 +1,14 @@
+from django.contrib import admin
+
 from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
-from django.contrib import admin
+
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/admin/', permanent=False)),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('registers.urls')),
-    url(r'^pos/', include('pos.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
