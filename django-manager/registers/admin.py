cadc70d32584e6f260a3ccb769f193c9b09d6e4e
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-04-29 19:03:25 +0200

    [register] the Django Admin calls PUSH_ADAPTERS with save_related()

--- django-manager/registers/admin.py
+++ django-manager/registers/admin.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 
 from .models import Product, Receipt, Sell
@@ -13,6 +14,17 @@ class ReceiptAdmin(admin.ModelAdmin):
     inlines = [
         SellInline,
     ]
+
+    def save_related(self, request, form, formsets, change):
+        """
+        Publish data to registered `Adapters`.
+        """
+        # save the model as usual
+        super().save_related(request, form, formsets, change)
+        instance = form.instance
+        # push data
+        for adapter in settings.PUSH_ADAPTERS:
+            adapter.push(instance)
 
 
 admin.site.register(Product)
